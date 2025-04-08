c89022e9ebc427eeadc10f94ab24edad82038a9d
ch-unine-058165 <ch-unine-058165@noto.epfl.ch>
2025-04-08 12:27:52 +0200

    fourth commit

---
+++ visualize.R
@@ -0,0 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .R
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: R
+#     language: R
+#     name: ir
+# ---
+
+x=c(1:50)
+y=c(1:50)
+plot(x,y)
+
+
